834f02339600a5c93e3513ae28911448bd6d51d3
ipagonis <gianniswade@gmail.com>
2021-12-04 22:07:59 +0200

    Adding Customer entity and CustomerService registration

---
+++ pfseven-eshop/src/main/java/com/pfseven/eshop/models/CategoryID.java
@@ -0,0 +1,5 @@
+package com.pfseven.eshop.models;
+
+public enum CategoryID {
+    B2C,B2B,B2G
+}
--- pfseven-eshop/src/main/java/com/pfseven/eshop/models/Customer.java
+++ pfseven-eshop/src/main/java/com/pfseven/eshop/models/Customer.java
@@ -1,4 +1,18 @@
 package com.pfseven.eshop.models;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+//@ToString(callSuper = false)
 public class Customer {
+    private Integer customerID;
+    private String firstName;
+    private String lastName;
+    private CategoryID categoryID;
 }
