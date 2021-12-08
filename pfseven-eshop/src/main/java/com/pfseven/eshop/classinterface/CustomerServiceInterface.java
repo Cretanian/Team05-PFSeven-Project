39e9137efb7ff17bae39d1313f107f3d644ceee8
Cretanian <stylianos.stamatakis@gmail.com>
2021-12-08 22:58:32 +0200

    interface did smthng... smh...

--- pfseven-eshop/src/main/java/com/pfseven/eshop/classinterface/CustomerServiceInterface.java
+++ pfseven-eshop/src/main/java/com/pfseven/eshop/classinterface/CustomerServiceInterface.java
@@ -3,9 +3,17 @@ package com.pfseven.eshop.classinterface;
 
 public interface CustomerServiceInterface {
 
-    /* ADD here explanation*/
+    /* This method creates a new customer
+    and then add this customer to the database.
+    This method returns the customer_ID in order
+    to continue with the order.    */
      Integer newCustomerInput();
 
-    /* ADD here explanation*/
-     Integer getCustomerIDfromDB();
+    /* This method requests the customer_ID from
+    console. If the customer_ID is not known then
+    requests the first name and the last name from
+    the console and querying the database to get the
+    customer_ID for this specific person.
+    This method returns the customer_ID. */
+    Integer getCustomerIDfromDB();
 }
