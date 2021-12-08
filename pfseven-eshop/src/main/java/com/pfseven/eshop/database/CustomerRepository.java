4280ac9d6e5540023ccb703e97519ceaa2e6cf5b
stathisAntonakis <stathis.ant@hotmail.com>
2021-12-08 19:51:42 +0200

    CustomerRepository

--- pfseven-eshop/src/main/java/com/pfseven/eshop/database/CustomerRepository.java
+++ pfseven-eshop/src/main/java/com/pfseven/eshop/database/CustomerRepository.java
@@ -1,4 +1,94 @@
 package com.pfseven.eshop.database;
 
+import com.pfseven.eshop.model.CategoryID;
+import com.pfseven.eshop.model.Customer;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class CustomerRepository {
+
+    private DatabasePF7Project dataBaseRef;
+
+
+    public CustomerRepository(DatabasePF7Project dataBaseRef){
+        this.dataBaseRef = dataBaseRef;
+    }
+
+    public int convertCategoryIDtoInt(CategoryID id) {
+        switch (id) {
+            case B2B:
+                return 1;
+            case B2C:
+                return 2;
+            case B2G:
+                return 3;
+            default:
+                return 0;
+        }
+    }
+
+    public CategoryID convertIntToCategoryID(int id) {
+        switch (id) {
+            case 1:
+                return CategoryID.B2B;
+            case 2:
+                return CategoryID.B2C;
+            case 3:
+                return CategoryID.B2G;
+            default:
+                return null;
+        }
+    }
+
+
+
+    public void insertNewCustomer (Customer newCustomer) throws SQLException {
+        PreparedStatement statement = this.dataBaseRef.getDBConnection().prepareStatement("INSERT INTO CUSTOMER(CUSTOMER_ID,FIRST_NAME,LAST_NAME,CATEGORY_ID) VALUES(NULL,?,?,?)");
+        statement.setString(1, newCustomer.getFirstName());
+        statement.setString(2, newCustomer.getLastName());
+        int categoryID = convertCategoryIDtoInt(newCustomer.getCategoryID());
+        statement.setString(3, String.valueOf(categoryID));
+        statement.executeUpdate();
+    }
+
+    public Customer getCustomerFromID(int customerID) throws SQLException {
+        Customer existedCustomer = new Customer();
+
+        PreparedStatement statement = this.dataBaseRef.getDBConnection().prepareStatement("SELECT * FROM CUSTOMER WHERE(CUSTOMER_ID = ?)");
+        statement.setString(1, String.valueOf(customerID));
+        ResultSet resultSet = statement.executeQuery();
+
+        existedCustomer.setFirstName(resultSet.getString("first_name"));
+        existedCustomer.setLastName(resultSet.getString("last_name"));
+
+        CategoryID categoryID = convertIntToCategoryID(resultSet.getInt("category_id"));
+        existedCustomer.setCategoryID(categoryID);
+
+        existedCustomer.setCustomerID(customerID);
+
+        return existedCustomer;
+    }
+
+    public Customer getCustomerFromName(String firstName, String lastName) throws SQLException {
+        Customer existedCustomer = new Customer();
+
+        PreparedStatement statement = this.dataBaseRef.getDBConnection().prepareStatement("SELECT * FROM CUSTOMER WHERE(FIRST_NAME = ? && LAST_NAME = ?)");
+        statement.setString(1, String.valueOf(firstName));
+        statement.setString(2, String.valueOf(lastName));
+        ResultSet resultSet = statement.executeQuery();
+
+        existedCustomer.setFirstName(resultSet.getString("first_name"));
+        existedCustomer.setLastName(resultSet.getString("last_name"));
+        existedCustomer.setCustomerID(resultSet.getInt("customer_id"));
+
+        CategoryID categoryID = convertIntToCategoryID(resultSet.getInt("category_id"));
+        existedCustomer.setCategoryID(categoryID);
+
+        return existedCustomer;
+
+    }
+
+
 }
